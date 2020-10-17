7bd15d65e4a16b99c49f5b52f84c3dbb6f8c87af
Viktor Faubl <faubl.viktor@gmail.com>
2020-10-18 00:07:16 +0100

    Init

---
+++ Math Application/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Math_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
